87203746b59867b5402ac42524ef552f9f21e321
lele94218 <terry.trxue@gmail.com>
2017-01-25 05:25:56 -0500

    add main test

--- main.c
+++ main.c
@@ -7,14 +7,28 @@
 #include "lwt_dispatch.h"
 
 void fun();
-int main();
+void fun1();
+void fun2();
+
 void fun()
 {
     printf("fun() \n");
 }
 
+void fun1()
+{
+    printf("fun1() \n");
+}
+
+void fun2()
+{
+    printf("fun2() \n");
+}
+
 int main()
 {
     lwt_t * t1 = lwt_create((void *) fun, NULL);
-    
+    lwt_t * t2 = lwt_create((void *) fun1, NULL);
+    lwt_t * t3 = lwt_create((void *) fun2, NULL);
+    printf("main finishing...\n");
 }
