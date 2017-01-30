24e1c5081441d768583a290b01d210fe0e8c2a71
lele94218 <terry.trxue@gmail.com>
2017-01-30 15:52:45 -0800

    test main

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 all: lwt
-
 lwt: lwt.c main.c lwt_dispatch.o
-    gcc -o main.o lwt.c main.c lwt_dispatch.o
+	gcc -o main.o lwt.c main.c lwt_dispatch.o
--- main.c
+++ main.c
@@ -10,33 +10,28 @@ lwt_t * t1;
 lwt_t * t2;
 lwt_t * t3;
 
-int fun1(int i);
+void fun1();
 void fun2();
 void fun3();
 void fun4();
 void fun5();
 
-int fun1(int i)
+void fun1()
 {
     printf("Fun1 starting ... \n");
-    i++;
     printf("Fun1 ending ... \n");
-    return i;
 }
 
 void fun2()
 {
     printf("Fun2 starting ... \n");
     lwt_yield(NULL);
-//    lwt_join(t1);
     printf("Fun2 ending ... \n");
 }
 
 void fun3()
 {
     printf("Fun3 starting ... \n");
-//    int i=lwt_join(t2);
-//    lwt_yield(NULL);
     printf("Fun3 ending ...  \n");
 }
 
@@ -56,22 +51,9 @@ void fun5()
 
 int main()
 {
-    int j=5;
-    int *p=j;
-    t1 = lwt_create((void *) fun1, p);
-//    lwt_yield(NULL);
+    t1 = lwt_create((void *) fun1, NULL);
     t2 = lwt_create((void *) fun2, NULL);
-//    lwt_yield(NULL);
     t3 = lwt_create((void *) fun3, NULL);
-//    lwt_yield(NULL);
-    lwt_join(t2);
-    lwt_t * t4 = lwt_create((void *) fun4, NULL);
-    lwt_t * t5 = lwt_create((void *) fun5, NULL);
-    
-//    lwt_yield(t1);
-//    lwt_yield(t2);
-//    lwt_yield(t3);
-//    lwt_yield(t5);
-    lwt_yield(NULL);
+    lwt_join(t1);
     printf("main finishing...\n");
 }
