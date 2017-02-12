d06dff53b6feb8c7ef8310a70ccbf4f88d6110d4
philipwhen <wzzh1203@gmail.com>
2017-02-11 21:11:04 -0500

    delete

--- test.c
+++ test.c
@@ -12,16 +12,6 @@ int to_send = 7;
 void fun()
 {
     printf("this is thread %d \n",lwt_id(lwt_current()));
-<<<<<<< HEAD
-    lwt_chan_t c = lwt_chan(0);
-    lwt_chan_t d = lwt_chan(0);
-    lwt_t t2 = lwt_create_chan((void *)fun1,c);
-//    lwt_snd(c, &to_send);
-    lwt_snd_chan(c,d);
-    int * result = lwt_rcv(d);
-    printf("get result%d \n", * result);
-=======
-
     lwt_chan_t c0 = lwt_chan(0);
     lwt_chan_t c1 = lwt_chan(0);
     lwt_chan_t c2 = lwt_chan(0);
@@ -49,22 +39,14 @@ void fun()
     received = (int) lwt_rcv(c_itself);
     printf("thread t1 has received: %d\n", received);
     lwt_chan_deref(c_itself);
->>>>>>> 8ea7170e9f3afc05579fcf12683acafa3214f782
 }
 void fun1(lwt_chan_t c)
 {
     printf("this is thread %d \n",lwt_id(lwt_current()));
-<<<<<<< HEAD
-//    int * result = lwt_rcv(c);
-    lwt_chan_t e = lwt_rcv_chan(c);
-    lwt_snd(e,&to_send);	
-//    printf("data %d received, end\n", *result);
-=======
     lwt_chan_t  using_channel = lwt_rcv_chan(c);
     lwt_snd(using_channel, (void *)lwt_id(lwt_current()));
     printf("thread %d has complete data sent to t1\n", lwt_id(lwt_current()));
     lwt_chan_deref(using_channel);
->>>>>>> 8ea7170e9f3afc05579fcf12683acafa3214f782
 }
 int main(int argc, char *argv[])
 {
