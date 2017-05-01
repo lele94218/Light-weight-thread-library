d4127e0a85b728d28fdbac2fe3ff11e974b1bdc4
lele94218 <terry.trxue@gmail.com>
2017-05-01 17:55:51 -0400

    fix header

--- lwt.h
+++ lwt.h
@@ -1,17 +1,21 @@
 #ifndef LWT_H
 #define LWT_H
+
 #include "lwt_list.h"
+
+#include "sl.h"
+#include <cos_defkernel_api.h>
 
 /* turn this on will enable error check for user input */
 #ifdef DEBUG
-#define printd(format, ...) printf("%s Line: %05d: " format, __FILE__, __LINE__, ## __VA_ARGS__)
+#define printd(format, ...) printc("%s Line: %05d: " format, __FILE__, __LINE__, ## __VA_ARGS__)
 #else
 #define printd(format, ...)
 #endif
 
 
 /* define size of stack for created thread */
-#define MAX_STACK_SIZE (1024 * 8)
+#define MAX_STACK_SIZE (100 * 1024 * 8)
 
 /* define NULL pointer */
 #define LWT_NULL NULL
@@ -71,6 +75,7 @@ enum block_status
     /* This is a zombie thread. */
     BLOCKED_SENDING,
 };
+
 
 
 /* define the context of a thread */
@@ -153,9 +158,6 @@ struct _lwt_t
     /* initial stack memory pointer, lowest address */
     unsigned int init_sp;
 
-    /* return value */
-//    void * last_word;
-
     /* Thread context */
     struct _lwt_context context;
     
@@ -166,13 +168,32 @@ struct _lwt_t
 
     /* thread join state */
     int nojoin;
+
+    /* kernel thread */
+    struct sl_thd * kthd;
 };
+
 typedef struct _lwt_t * lwt_t;
 
 
 /* define a function pointer */
 typedef void * (*lwt_fn_t)(void *);
 typedef void * (*lwt_chan_fn_t)(lwt_chan_t);
+
+
+struct _lwt_cap
+{
+    int block_counter;
+    int lwt_counter;
+    int zombie_counter;
+    int nrcving;
+    int nsnding;
+    lwt_t current_thread;
+    lwt_t old_thread;
+    struct list_head run_queue;
+    struct list_head recycle_queue;
+};
+
 
 
 
@@ -185,6 +206,7 @@ int lwt_yield(lwt_t strong_thread);
 lwt_t lwt_current();
 int lwt_id(lwt_t input_thread);
 lwt_t lwt_current();
+void lwt_init_cap(struct _lwt_cap *);
 
 /* --------------------------- Function declaration for lwt thread channel operation --------------------------- */
 lwt_chan_t lwt_chan (int sz);
@@ -204,6 +226,10 @@ lwt_chan_t lwt_cgrp_wait (lwt_cgrp_t cgrp);
 void lwt_chan_mark_set(lwt_chan_t, void *);
 void *lwt_chan_mark_get(lwt_chan_t);
 
+int lwt_kthd_create(lwt_fn_t fn, void * data, lwt_chan_t c);
+
+
+
 /* --------------------------- debugging function --------------------------- */
 void print_queue_content(enum lwt_info_t);
 int lwt_info(enum lwt_info_t t);
@@ -219,6 +245,7 @@ extern int nsnding;
 extern lwt_t current_thread;
 extern struct list_head run_queue;
 extern struct list_head recycle_queue;
+extern struct cos_compinfo *ci;
 
 
 #endif
